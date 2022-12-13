f8133bb9a49f052835450fc0f68a32dec277d2d3
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2022-12-13 11:58:27 +0800

    playing with strip function

---
+++ strip.py
@@ -0,0 +1,6 @@
+favorite_language = " python"
+word = favorite_language
+a = favorite_language.rstrip()
+b = favorite_language.lstrip()
+c = favorite_language.strip()
+print("word = "+word+"\n"+"a = "+a+"\n"+"b = "+b+"\n"+"c = "+c)
